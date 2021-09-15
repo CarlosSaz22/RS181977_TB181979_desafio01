d689b419f5807b773e980ed918eadea1ae390aec
CarlosSaz22 <61812538+CarlosSaz22@users.noreply.github.com>
2021-09-14 21:24:45 -0600

    Creación de modelo TipoCuentaBancaria y modificación de clase Banco.

---
+++ RS181977_TB181979_desafío01/Models/TipoCuentaBancaria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RS181977_TB181979_desafío01.Models
+{
+    public class TipoCuentaBancaria
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Display(Name = "Tipo de cuenta bancaria")]
+        [StringLength(150)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Campo tipo de cuenta bancaria requerido")]
+        public String Tipo_Transaccion { get; set; }
+
+
+
+        [Display(Name = "Activo")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Campo activo requerido")]
+        public Boolean Activo { get; set; }
+
+    }
+}
